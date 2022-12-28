f58893ac3dc168632cff73f4444fb324a1583ce4
razorSainath <sainath.reddy@razorlabz.com>
2022-12-28 19:13:34 +0530

    final

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-        "~/node_modules/bootstrap/dist/css/bootstrap.min.css",
-        "~/assets/css/bootstrap.min.css",
-        "~/assets/css/app.min.css"
-    ],
-    plugins: [
-        { src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", mode: "client" }
-    ],
-})
+  css: [ "~/assets/styles/main.scss", "@/assets/css/app.min.css"],
+});
