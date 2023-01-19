1dcb67503348d5bd288a3194176cd40f0745b25d
nibhashrazorlabz <nibhash@razorlabz.com>
2023-01-19 19:14:12 +0530

    ui enhancement

--- middleware/rough.js
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    console.log(to,"here comes the middleware",from)
-    // if (to.params.id === '1') {
-    //   return abortNavigation()
-    // }
-    // return navigateTo('/')
-  })
