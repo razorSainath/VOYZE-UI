cbf03ebc402c2caba11e6012a1c44900de761810
razorSainath <sainath.reddy@razorlabz.com>
2023-01-20 12:46:39 +0530

    inactive active in sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,38 +1,154 @@
 <script setup>
+import {ref} from 'vue'
+let dashboardColour = ref("white");
+let workspaceColour = ref("white");
+let leadersColour = ref("white");
+let analyticsColour = ref("white");
+let postsColour = ref("white");
+let curationColour = ref("white");
+let categoryColour = ref("white");
+let usersColour = ref("white");
+let rolesColour = ref("white");
+let partnersColour = ref("white");
+
+
 const redirectTo = (page) => {
+
+//iam gonna put the variable in backround colour parameter & set the respective values as whight-black button is clicked
+//first implement for the 1 button 
 
 switch (page) {
   case 'dashboard':
   navigateTo("/dashboard")
-
+  dashboardColour.value = "#009cd2";
+  workspaceColour = ref("white");
+ leadersColour = ref("white");
+ analyticsColour = ref("white");
+ postsColour = ref("white");
+ curationColour = ref("white");
+ categoryColour = ref("white");
+ usersColour = ref("white");
+ rolesColour = ref("white");
+ partnersColour = ref("white");
     break
   case 'workspace':
   navigateTo("/organizations/sasdf/workspaces")
-
+  dashboardColour = ref("white");
+  workspaceColour.value = "#009cd2";
+ leadersColour = ref("white");
+ analyticsColour = ref("white");
+ postsColour = ref("white");
+ curationColour = ref("white");
+ categoryColour = ref("white");
+ usersColour = ref("white");
+ rolesColour = ref("white");
+ partnersColour = ref("white");
   break
   case 'leaders':
   navigateTo("/leaders")
+  dashboardColour = ref("white");
+  workspaceColour = ref("white");
+ leadersColour.value = "#009cd2";
+ analyticsColour = ref("white");
+ postsColour = ref("white");
+ curationColour = ref("white");
+ categoryColour = ref("white");
+ usersColour = ref("white");
+ rolesColour = ref("white");
+ partnersColour = ref("white");
     break;
   case 'analytics':
   navigateTo("/analytics")
+  dashboardColour = ref("white");
+  workspaceColour = ref("white");
+ leadersColour = ref("white");
+ analyticsColour.value = "#009cd2";
+ postsColour = ref("white");
+ curationColour = ref("white");
+ categoryColour = ref("white");
+ usersColour = ref("white");
+ rolesColour = ref("white");
+ partnersColour = ref("white");
     break;
   case 'posts':
   navigateTo("/posts")
+  dashboardColour = ref("white");
+  workspaceColour = ref("white");
+ leadersColour = ref("white");
+ analyticsColour = ref("white");
+ postsColour.value = "#009cd2";
+ curationColour = ref("white");
+ categoryColour = ref("white");
+ usersColour = ref("white");
+ rolesColour = ref("white");
+ partnersColour = ref("white");
     break;
   case 'curation':
   navigateTo("/curation")
+  dashboardColour = ref("white");
+  workspaceColour = ref("white");
+ leadersColour = ref("white");
+ analyticsColour = ref("white");
+ postsColour = ref("white");
+ curationColour.value = "#009cd2";
+ categoryColour = ref("white");
+ usersColour = ref("white");
+ rolesColour = ref("white");
+ partnersColour = ref("white");
     break;
   case 'category':
   navigateTo("/categories")
+  dashboardColour = ref("white");
+  workspaceColour = ref("white");
+ leadersColour = ref("white");
+ analyticsColour = ref("white");
+ postsColour = ref("white");
+ curationColour = ref("white");
+ categoryColour.value = "#009cd2";
+ usersColour = ref("white");
+ rolesColour = ref("white");
+ partnersColour = ref("white");
     break;
   case 'user-management':
   navigateTo("/users")
+  dashboardColour = ref("white");
+  workspaceColour = ref("white");
+ leadersColour = ref("white");
+ analyticsColour = ref("white");
+ postsColour = ref("white");
+ curationColour = ref("white");
+ categoryColour=  ref("white");
+ usersColour.value = "#009cd2";
+ rolesColour = ref("white");
+ partnersColour = ref("white");
+
     break;
   case 'role-management':
   navigateTo("/roles")
+  dashboardColour = ref("white");
+  workspaceColour = ref("white");
+ leadersColour = ref("white");
+ analyticsColour = ref("white");
+ postsColour = ref("white");
+ curationColour = ref("white");
+ categoryColour=  ref("white");
+ usersColour = ref("white");
+ rolesColour.value = "#009cd2";
+ partnersColour = ref("white");
     break;
   case 'partners-management':
   navigateTo("/partners")
+
+  dashboardColour = ref("white");
+  workspaceColour = ref("white");
+ leadersColour = ref("white");
+ analyticsColour = ref("white");
+ postsColour = ref("white");
+ curationColour = ref("white");
+ categoryColour=  ref("white");
+ usersColour = ref("white");
+ rolesColour = ref("white");
+ partnersColour.value = "#009cd2";
     break;
 }
 };
@@ -71,39 +187,39 @@ const isSidebarOpen = useState("isSidebarOpen",()=>true)
         <ul class="metismenu list-unstyled" id="side-menu">
           <li class="menu-title">Menu</li>
 
-          <li class="nav-item">
-            <a @click="redirectTo('dashboard')" class="waves-effect nav-link active">
+          <li class="nav-item" :style="{'background-color': dashboardColour}">
+            <a @click="redirectTo('dashboard')"  class="waves-effect nav-link active">
               <i class="fa-solid fa-dashboard"></i>
               <span>Dashboard</span>
             </a>
           </li>
           <li class="menu-title">Sub-menu</li>
-          <li>
+          <li :style="{'background-color': workspaceColour}">
             <a @click="redirectTo('workspace')" class="waves-effect">
               <i class="fa-solid fa-folder-tree"></i>
               <span>Workspace</span>
             </a>
           </li>
-          <li>
+          <li :style="{'background-color': leadersColour}">
             <a @click="redirectTo('leaders')" class="waves-effect">
               <i class="fa-solid fa-user"></i>
               <span>Leaders</span>
             </a>
           </li>
-          <li>
+          <li :style="{'background-color': analyticsColour}">
             <a @click="redirectTo('analytics')" class="waves-effect">
               <i class="fa-solid fa-chart-simple"></i>
               <span>Analytics</span>
             </a>
           </li>
           <li class="menu-title">Content</li>
-          <li>
+          <li :style="{'background-color': postsColour}">
             <a @click="redirectTo('posts')" class="waves-effect">
               <i class="fa-solid fa-envelopes-bulk"></i>
               <span>Posts</span>
             </a>
           </li>
-          <li>
+          <li :style="{'background-color': curationColour}">
             <a @click="redirectTo('curation')" class="waves-effect">
               <i class="fa-solid fa-file-lines"></i>
               <span>Curation</span>
@@ -112,27 +228,27 @@ const isSidebarOpen = useState("isSidebarOpen",()=>true)
 
           <li class="menu-title">Manage</li>
 
-          <li>
+          <li :style="{'background-color': categoryColour}">
             <a @click="redirectTo('category')" class="waves-effect">
               <i class="fa-solid fa-bars-progress"></i>
               <span>Category</span>
             </a>
           </li>
 
-          <li>
+          <li :style="{'background-color': usersColour}">
             <a @click="redirectTo('user-management')" class="waves-effect">
               <i class="fa-solid fa-users"></i>
               <span>Users</span>
             </a>
           </li>
 
-          <li>
+          <li :style="{'background-color': rolesColour}">
             <a @click="redirectTo('role-management')" class="waves-effect">
               <i class="fa-solid fa-list-check"></i>
               <span>Roles</span>
             </a>
           </li>
-          <li>
+          <li :style="{'background-color': partnersColour}">
             <a @click="redirectTo('partners-management')" class="waves-effect">
               <i class="fa-solid fa-handshake"></i>
               <span>Partners</span>
--- pages/rough.vue
+++ pages/rough.vue
@@ -1,2 +1,29 @@
-<template></template>
+<script setup>
 
+import { ref } from "vue";
+const roughVariable = ref("blue");
+
+const toggleSidebar = computed(() => {
+  console.log("clicked");
+  roughVariable.value = "red";
+});
+//multiple objects will be created of each tab in sidebar
+
+
+
+//here whole script is getting executed only when the script of that page is changed 
+//computed properties are executed only when the dependent variable change
+
+</script>
+
+<template>
+  <button @click="toggleSidebar">golu</button>
+  <div :style="{'background-color': roughVariable ,'text-transform' : uppercase}">first</div>
+  <div :style="{'background-color': roughVariable}">secound</div>
+  <div :style="{'background-color': roughVariable}">third</div>
+  <div :style="{'background-color': roughVariable}">fourth</div>
+
+
+<!-- <h1>hello</h1> -->
+
+</template>
