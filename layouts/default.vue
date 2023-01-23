7be918490c93dd90fb2704f0dd6f920c9ff70033
razorSainath <sainath.reddy@razorlabz.com>
2023-01-23 13:25:40 +0530

    responsive implmentation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,58 @@
 <template>
-    <NavBar/>
-    <Sidebar/>
-    
-    <div class="main-content" >
-    <div class="page-content" >
-      <div class="container-fluid" >
-        
-<slot/>
+  <NavBar />
 
+  <div class="app">
+	<Sidebar1 />
+    <div class="page-content">
+      <div class="container-fluid">
+        <slot />
       </div>
     </div>
   </div>
 
-    <Footer/>
+  <Footer />
 </template>
 
+<style lang="scss">
+:root {
+  --primary: #4ade80;
+  --primary-alt: #22c55e;
+  --grey: #64748b;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #f1f5f9;
+  --sidebar-width: 300px;
+}
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira sans", sans-serif;
+}
 
+body {
+  background: var(--light);
+}
 
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+.app {
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+
+    @media (max-width: 1024px) {
+      padding-left: 6rem;
+    }
+  }
+}
+</style>
