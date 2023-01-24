948c7cde95ed8b7a2ed86c7dc65024a62e95e88a
nibhashrazorlabz <nibhash@razorlabz.com>
2023-01-24 18:29:32 +0530

    ui enhancement

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -397,15 +397,12 @@ const isSidebarOpen = useState("isSidebarOpen",()=>true)
   </div>
 
   
-  <div class="vertical-menu" v-else="isSidebarOpen" style="display: flex;z-index:1;">
+  <div class="vertical-menu-collapsed" v-else style="display: flex;z-index:1;">
     <!-- LOGO -->
     <div class="navbar-brand-box">
       <a href="index.html" class="logo logo-dark">
         <span class="logo-sm" style="position: relative; right: 15px">
-          <img src="assets/images/logo-sm.png" alt="" height="22" />
-        </span>
-        <span class="logo-lg">
-          <img src="~/assets/images/logo-light.svg" alt="" height="20" />
+          <img src="assets/images/logo-sm-removebg-preview.png" alt="" height="22" />
         </span>
       </a>
     </div>
@@ -416,73 +413,58 @@ const isSidebarOpen = useState("isSidebarOpen",()=>true)
       <div id="sidebar-menu">
         <!-- Left Menu Start -->
         <ul class="metismenu list-unstyled" id="side-menu">
-          <li class="menu-title">Menu</li>
 
           <li>
             <a @click="redirectTo('dashboard')" class="waves-effect">
               <i class="fa-solid fa-dashboard"></i>
-              <span>Dashboard</span>
             </a>
           </li>
-          <li class="menu-title">Sub-menu</li>
           <li>
             <a @click="redirectTo('workspace')" class="waves-effect">
               <i class="fa-solid fa-folder-tree"></i>
-              <span>Workspace</span>
             </a>
           </li>
           <li>
             <a @click="redirectTo('leaders')" class="waves-effect">
               <i class="fa-solid fa-user"></i>
-              <span>Leaders</span>
             </a>
           </li>
           <li>
             <a @click="redirectTo('analytics')" class="waves-effect">
               <i class="fa-solid fa-chart-simple"></i>
-              <span>Analytics</span>
             </a>
           </li>
-          <li class="menu-title">Content</li>
           <li>
             <a @click="redirectTo('posts')" class="waves-effect">
               <i class="fa-solid fa-envelopes-bulk"></i>
-              <span>Posts</span>
             </a>
           </li>
           <li>
             <a @click="redirectTo('curation')" class="waves-effect">
               <i class="fa-solid fa-file-lines"></i>
-              <span>Curation</span>
             </a>
           </li>
-
-          <li class="menu-title">Manage</li>
 
           <li>
             <a @click="redirectTo('category')" class="waves-effect">
               <i class="fa-solid fa-bars-progress"></i>
-              <span>Category</span>
             </a>
           </li>
 
           <li>
             <a @click="redirectTo('user-management')" class="waves-effect">
               <i class="fa-solid fa-users"></i>
-              <span>Users</span>
             </a>
           </li>
 
           <li>
             <a @click="redirectTo('role-management')" class="waves-effect">
               <i class="fa-solid fa-list-check"></i>
-              <span>Roles</span>
             </a>
           </li>
           <li>
             <a @click="redirectTo('partners-management')" class="waves-effect">
               <i class="fa-solid fa-handshake"></i>
-              <span>Partners</span>
             </a>
           </li>
         </ul>
