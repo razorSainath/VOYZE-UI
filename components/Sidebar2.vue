831ad1ad0d2ad8fe0512f3f869edffd29c327472
razorSainath <sainath.reddy@razorlabz.com>
2023-01-30 14:03:46 +0530

    active-inactive

--- components/Sidebar2.vue
+++ components/Sidebar2.vue
@@ -1,9 +1,18 @@
 <script setup>
-import { ref } from "vue";
 
-const dashboard = ref(false);
-// const  = ref(true)
+import { ref } from "vue";
 let isSidebarOpen = useState("isSidebarOpen", () => false);
+let dashboard = ref(false);
+let workspace = ref(false);
+let leaders = ref(false);
+let analytics = ref(false);
+let posts = ref(false);
+let curation = ref(false);
+let category = ref(false);
+let users = ref(false);
+let roles = ref(false);
+let partners = ref(false);
+
 const redirectTo = (page) => {
   //iam gonna put the variable in backround colour parameter & set the respective values as whight-black button is clicked
   //first implement for the 1 button
@@ -11,59 +20,167 @@ const redirectTo = (page) => {
   switch (page) {
     case "dashboard":
       navigateTo("/dashboard");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
+
+      dashboard.value = true;
+      workspace.value = false;
+      leaders.value = false;
+      analytics.value = false;
+      posts.value = false;
+      curation.value = false;
+      category.value = false;
+      users.value = false;
+      roles.value = false;
+      partners.value = false;
       break;
     case "workspace":
       navigateTo("/organizations/sasdf/workspaces");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
+
+      dashboard.value = false;
+      workspace.value = true;
+      leaders.value = false;
+      analytics.value = false;
+      posts.value = false;
+      curation.value = false;
+      category.value = false;
+      users.value = false;
+      roles.value = false;
+      partners.value = false;
+
       break;
     case "leaders":
       navigateTo("/leaders");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
 
+      dashboard.value = false;
+      workspace.value = false;
+      leaders.value = true;
+      analytics.value = false;
+      posts.value = false;
+      curation.value = false;
+      category.value = false;
+      users.value = false;
+      roles.value = false;
+      partners.value = false;
       break;
     case "analytics":
       navigateTo("/analytics");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
+
+      dashboard.value = false;
+      workspace.value = false;
+      leaders.value = false;
+      analytics.value = true;
+      posts.value = false;
+      curation.value = false;
+      category.value = false;
+      users.value = false;
+      roles.value = false;
+      partners.value = false;
 
       break;
     case "posts":
       navigateTo("/posts");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
+
+      dashboard.value = false;
+      workspace.value = false;
+      leaders.value = false;
+      analytics.value = false;
+      posts.value = true;
+      curation.value = false;
+      category.value = false;
+      users.value = false;
+      roles.value = false;
+      partners.value = false;
 
       break;
     case "curation":
       navigateTo("/curation");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
+
+      dashboard.value = false;
+      workspace.value = false;
+      leaders.value = false;
+      analytics.value = false;
+      posts.value = false;
+      curation.value = true;
+      category.value = false;
+      users.value = false;
+      roles.value = false;
+      partners.value = false;
 
       break;
     case "category":
       navigateTo("/categories");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
+
+      dashboard.value = false;
+      workspace.value = false;
+      leaders.value = false;
+      analytics.value = false;
+      posts.value = false;
+      curation.value = false;
+      category.value = true;
+      users.value = false;
+      roles.value = false;
+      partners.value = false;
 
       break;
     case "user-management":
       navigateTo("/users");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
+
+      dashboard.value = false;
+      workspace.value = false;
+      leaders.value = false;
+      analytics.value = false;
+      posts.value = false;
+      curation.value = false;
+      category.value = false;
+      users.value = true;
+      roles.value = false;
+      partners.value = false;
 
       break;
     case "role-management":
       navigateTo("/roles");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
 
+
+      dashboard.value = false;
+      workspace.value = false;
+      leaders.value = false;
+      analytics.value = false;
+      posts.value = false;
+      curation.value = false;
+      category.value = false;
+      users.value = false;
+      roles.value = true;
+      partners.value = false;
       break;
     case "partners-management":
       navigateTo("/partners");
-      isSidebarOpen.value = false
+      isSidebarOpen.value = false;
 
+
+      dashboard.value = false;
+      workspace.value = false;
+      leaders.value = false;
+      analytics.value = false;
+      posts.value = false;
+      curation.value = false;
+      category.value = false;
+      users.value = false;
+      roles.value = false;
+      partners.value = true;
       break;
   }
 };
 //as the user clicks the button on dashboard
 //the it is redirected to that page & also the button text need to be styled differently
 // i need to create a function which will create take in the page & hilight
-
-
 
 // const panels = document.querySelectorAll('.li')
 // console.log(panels);
@@ -95,42 +212,42 @@ const redirectTo = (page) => {
         <ul class="metismenu list-unstyled mm-show" id="side-menu">
           <li class="menu-title">Menu</li>
 
-          <li>
+          <li :class="`${dashboard ? 'mm-active' : ''}`">
             <a @click="redirectTo('dashboard')" class="waves-effect">
-                <i class="fa-solid fa-dashboard"></i>
+              <i class="fa-solid fa-dashboard"></i>
               <span>Dashboard</span>
             </a>
           </li>
 
           <li class="menu-title">Sub-menu</li>
-          <li class="mm-active">
+          <li :class="`${workspace ? 'mm-active' : ''}`">
             <a @click="redirectTo('workspace')" class="waves-effect">
               <i class="fa-solid fa-folder-tree"></i>
               <span>Workspace</span>
             </a>
           </li>
 
-          <li>
+          <li :class="`${leaders ? 'mm-active' : ''}`">
             <a @click="redirectTo('leaders')" class="waves-effect">
               <i class="fa-solid fa-user"></i>
               <span>Leaders</span>
             </a>
           </li>
 
-          <li>
+          <li :class="`${analytics ? 'mm-active' : ''}`">
             <a @click="redirectTo('analytics')" class="waves-effect">
               <i class="fa-solid fa-chart-simple"></i>
               <span>Analytics</span>
             </a>
           </li>
           <li class="menu-title">Content</li>
-          <li>
+          <li :class="`${posts ? 'mm-active' : ''}`">
             <a @click="redirectTo('posts')" class="waves-effect">
               <i class="fa-solid fa-envelopes-bulk"></i>
               <span>Posts</span>
             </a>
           </li>
-          <li>
+          <li :class="`${curation ? 'mm-active' : ''}`">
             <a @click="redirectTo('curation')" class="waves-effect">
               <i class="fa-solid fa-file-lines"></i>
               <span>Curation</span>
@@ -139,27 +256,27 @@ const redirectTo = (page) => {
 
           <li class="menu-title">Manage</li>
 
-          <li>
+          <li :class="`${category ? 'mm-active' : ''}`">
             <a @click="redirectTo('category')" class="waves-effect">
               <i class="fa-solid fa-bars-progress"></i>
               <span>Category</span>
             </a>
           </li>
 
-          <li>
+          <li :class="`${users ? 'mm-active' : ''}`">
             <a @click="redirectTo('user-management')" class="waves-effect">
               <i class="fa-solid fa-users"></i>
               <span>Users</span>
             </a>
           </li>
 
-          <li>
+          <li :class="`${roles ? 'mm-active' : ''}`">
             <a @click="redirectTo('role-management')" class="waves-effect">
               <i class="fa-solid fa-list-check"></i>
               <span>Roles</span>
             </a>
           </li>
-          <li>
+          <li :class="`${partners ? 'mm-active' : ''}`">
             <a @click="redirectTo('partners-management')" class="waves-effect">
               <i class="fa-solid fa-handshake"></i>
               <span>Partners</span>
