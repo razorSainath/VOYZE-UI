8a6f36a87a113fa0ed49d20dff013866d4120da0
razorSainath <112610055+razorSainath@users.noreply.github.com>
2023-02-07 13:49:00 +0530

    done nav-bar

--- pages/rough.vue
+++ pages/rough.vue
@@ -1,12 +1,52 @@
+
+<script setup>
+definePageMeta({
+  layout: "empty",
+});
+</script>
+  <!-- @click="options=!options" -->
+
 <template>
 
-  <h1>About</h1>
-		<p>This is the about page</p>
+                  <!-- item-->
+                  <div class="float-right mt- w-[200px] h-[130px]">
+            <div
+              class="bg-white dark:bg-gray-800 rounded-lg shadow-lg h-full p-6"
+            >
+              <div class="flex flex-wrap flex-row items-center">
+                <div class="flex-shrink max-w-full">
+                  <div class="flex flex-col">
+                    <div class="">
+                      <a class="dropdown-item" href="#"
+                        ><i
+                          class="fa-solid fa-user-circle font-size-18 align-middle text-muted me-1"
+                        ></i>
+                        <span class="align-middle">View Profile</span></a
+                      >
+                    </div>
+                    <div class="">
+                      <a class="dropdown-item" href="#"
+                        ><i
+                          class="fa-sharp fa-solid fa-display font-size-18 align-middle me-1 text-muted"
+                        ></i>
+                        <span class="align-middle">Lock screen</span></a
+                      >
+                    </div>
+                    <div class="">
+                      <a class="dropdown-item" href="#"
+                        ><i
+                          class="fa-solid fa-sign-out-alt font-size-18 align-middle me-1 text-muted"
+                        ></i>
+                        <span class="align-middle">Sign out</span></a
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+                    <!-- item-->
 
 </template>
 
-<script setup>
-//basically nothing but pseudo code is written here
-//new sidebar default layout is part of rough page 
-//will make it from scrath & learn several thing on html & css
-</script>
+
